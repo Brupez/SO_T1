38c9e7c02c1fd23ca54314df31ab9806d820bfbd
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-10-20 01:17:53 +0100

    Argument flags support

---
+++ spacecheck.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+orderByName=false # Disabled
+maxDate=$(date)   # Disabled
+outputLimit=0     # Disabled
+filter=".*"       # All files
+reverse=false     # Disabled
+minFileSize=0     # All files; in bytes
+
+while getopts "ad:l:n:rs:" o; do
+    case "${o}" in
+    a)
+        orderByName=true
+        ;;
+    d)
+        maxDate=${OPTARG}
+        ;;
+    l)
+        outputLimit=${OPTARG}
+        ;;
+    n)
+        filter=${OPTARG}
+        ;;
+    r)
+        reverse=true
+        ;;
+    s)
+        minFileSize=${OPTARG}
+        ;;
+    *) ;;
+    esac
+done
